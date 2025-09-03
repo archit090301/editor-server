c11e4e325cdf3b3e29fc776806655d08fefb7a7f
archit090301 <valoarchit@gmail.com>
2025-09-03 18:26:59 -0400

    Update server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ const io = new Server(server, {
     methods: ['GET', 'POST'],
     credentials: true
   },
-  transports: ['websocket', 'polling']
+  transports: ['websocket']
 });
 
 // Collaboration socket logic
